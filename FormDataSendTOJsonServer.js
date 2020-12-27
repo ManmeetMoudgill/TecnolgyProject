078a2d109e5855898bae2762f7fa9d5bffee0b87
manmeet <manmeetmoudgill123456789@gmail.com>
2020-12-27 19:51:37 +0100

    adding file tou tecnologie progetto

---
+++ FormDataSendTOJsonServer.js
@@ -0,0 +1,147 @@
+
+function Info(givenNome,givenCognome,givendataDiNascita,givenLuogoNascita,givenNumeroTel,
+    givenemail,givencap,giveneta,
+    givenresidenza,givenNumeroFam,givenIntereFam){
+    this.nome=givenNome;
+    this.cognome=givenCognome;
+    this.dataDiNascita=givendataDiNascita;
+    this.luogonascita=givenLuogoNascita;
+    this.numerotel=givenNumeroTel;
+    this.email=givenemail;
+    this.cap=givencap;
+    this.eta=giveneta;
+    this.residenza=givenresidenza;
+    this.numerofam=givenNumeroFam;
+    this.interessefam=givenIntereFam;
+
+   
+}
+
+
+
+
+let submitButtonInfo=document.getElementById('submitButtonInfo');
+submitButtonInfo.addEventListener('click',(event)=>{
+
+    event.preventDefault();
+    let nomeInput=document.getElementById('nome').value;
+    let CognomeInput=document.getElementById('cognome').value;
+    let dataDiNascitaInput=document.getElementById('dataDiNascita').value;
+    let luogoDiNascitaInput=document.getElementById('luogoDiNascita').value;
+    let numeroTelefonoInput=document.getElementById('numeroDiTelefono').value;
+    let emailInput=document.getElementById('email').value;
+    let capInput=document.getElementById('cap').value;
+    let etaInput=document.getElementById('eta').value;
+    let ResidenzaInput=document.getElementById('residenza').value;
+    let numeroFamInput=document.getElementById('numeroFamiliare').value;
+    let InteressiFamInput=document.getElementById('interessiFamiliare').value;
+    
+    let newObject=new Info(nomeInput,CognomeInput,dataDiNascitaInput,luogoDiNascitaInput,numeroTelefonoInput,
+        emailInput,capInput,etaInput,ResidenzaInput,numeroFamInput,InteressiFamInput);
+    console.log(newObject);
+    console.log(newObject.nome);
+
+
+    
+      if(nomeInput==="" || cognome==="" || dataDiNascitaInput==="" || luogoDiNascitaInput==="" || numeroTelefonoInput==" " || emailInput===""
+      ||capInput==="" || emailInput==="" || ResidenzaInput==="" || numeroFamInput==="" || InteressiFamInput===""){
+          let messageDiv=document.getElementById('messageDiv');
+          let stringMessage=`  <div class="alert alert-danger alert-dismissible" role="alert">
+          <span type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></span>
+          <strong>Attenzione!</strong> Non puoi Lasciare i campi in bianco.
+        </div>`;
+         messageDiv.innerHTML=stringMessage;
+      }else{
+        let url='http://localhost:3000/posts';
+        let params={
+          method:'POST',
+          headers:{'Content-Type':'application/json'},
+          body:JSON.stringify(newObject)
+        }
+       
+       
+        fetch(url,params).then((response)=>{
+         return response.json();
+        }).then((data)=>{
+         console.log(data);
+         alert('Data Has been sent Succesfully')
+        }).catch(()=>console.error("SomeThing went wrong")) 
+       
+      }
+    })
+
+    
+let ShowButtonDiv=document.getElementById('ShowButtonInfo');
+ShowButtonDiv.addEventListener('click',callGetData);
+
+//function used to call the function getDatafromServer for getting the data from the server
+function callGetData(){
+  getDatafromServer();
+}
+
+
+
+
+
+
+
+let j=1;
+function getDatafromServer(){
+  if(j===1){
+  let url="http://localhost:3000/posts";
+  
+  //using fetch api to getting the data;
+  
+  fetch(url).then((response)=>{
+    return response.json();
+  }).then((data)=>{
+    let dataFetched=data;
+    console.log(dataFetched)
+    console.log(dataFetched.length)
+    let stringTable="";
+    for(i=0;i<dataFetched.length;i++){
+      stringTable=`
+      <tr class="table-striped">
+              <td>${dataFetched[i].nome}</td>
+              <td>${dataFetched[i].cognome}</td>
+              <td>${dataFetched[i].DataDiNascita}</td>
+              <td>${dataFetched[i].Email}</td>
+              <td>${dataFetched[i].LuogoDiNascita}</td>
+              <td>${dataFetched[i].NumeroDiTelefono}</td>
+              <td>${dataFetched[i].Cap}</td>
+              <td>${dataFetched[i].Eta}</td>
+              <td>${dataFetched[i].Residenza}</td>
+              <td>${dataFetched[i].NumeroFamiliare}</td>
+              <td>${dataFetched[i].InteresseFamiliare}</td>
+              </tr>
+              </table>`;
+              
+              tableBody=document.getElementById('TableBody');
+              tableBody.innerHTML+=stringTable;
+            }
+})
+}else{
+  let messageDiv=document.getElementById('messageDiv');
+          let stringMessage=`  <div class="alert alert-danger alert-dismissible" role="alert">
+          <span type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></span>
+          <strong>Attenzione!</strong> Stai gia vedendo i clienti!! Lista finito..
+        </div>`;
+         messageDiv.innerHTML=stringMessage;
+}
+j++;
+}
+
+
+
+//function used to hide the div when the hide button is presed
+let HideButtonDiv=document.getElementById('HideButtonInfo');
+HideButtonDiv.addEventListener('click',()=>{
+
+  divMainTable=document.getElementById('divTable').style.display="none";
+
+})
+
+let showButtonTabella=document.getElementById('showButtonTabellaInfo');
+showButtonTabella.addEventListener('click',()=>{
+  divMainTable=document.getElementById('divTable').style.display="block";
+})
